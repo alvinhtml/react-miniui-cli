0b6bcb4f89749bf2d22d53b03395a39c8e7798f9
alvin <alvinhtml@gmail.com>
2019-12-03 17:32:47 +0800

    fixed bug for project name

--- index.js
+++ index.js
@@ -10,23 +10,20 @@ program
   .description('react-miniui 项目的 CLI.');
 
 program
-  .command('miniui-cli <project>')
-  .action(function(tpl, project) {
-
-    log.info('目前 react-miniui-cli 支持以下模板：');
-    log.info('使用例子：miniui-cli myProject');
+  .command('create <project>')
+  .action(function(project) {
 
     if (project) {
         let pwd = shell.pwd();
 
-        log.info(`正在拉取代码，下载位置：${pwd}/${project}/ ...`);
+        log.info(`正在拉取代码，下载位置：${pwd}/${project}/...`);
 
         clone(`https://github.com/alvinhtml/react-miniui-cli-repository.git`, `${pwd}/${project}`, null, function() {
             shell.rm('-rf', pwd + `/${project}/.git`);
-            log.info('模板创建完成！使用： npm install && npm run start');
+            log.info('模板创建完成，使用： npm install && npm run start 开始。');
         });
     } else {
-        log.error('正确命令例子：miniui-cli myProject');
+        log.error('正确命令例子：miniui-cli create myProject');
     }
   });
 
